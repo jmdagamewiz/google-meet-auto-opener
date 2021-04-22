2fdcbb11538c8b9e1b22063deaa877c8149c6264
jmdagamewiz <75661560+jmdagamewiz@users.noreply.github.com>
2021-04-22 21:05:59 +0800

    Finish front-end part of app
    Finished all front-end features and refactored code

--- gui.py
+++ gui.py
@@ -7,19 +7,24 @@ import sys
 import math
 
 
+class Room:
+
+    def __init__(self, name, code, time, days_list):
+        self.name = name
+        self.code = code
+        self.time = time
+        self.days_list = days_list
+
+
 class EditRoomFrame(QFrame):
 
-    def __init__(self, room_title, room_code, room_time, room_days_list):
+    def __init__(self, room):
         super().__init__()
 
-        self.room_title = room_title
-        self.room_code = room_code
-        self.room_time = room_time
-        self.room_days_list = room_days_list
+        self.room = room
 
         self.setObjectName("main")
         self.setStyleSheet("""
-        
             QFrame#main {
                 background-color: white;
                 border: 1px solid gray;
@@ -59,45 +64,45 @@ class EditRoomFrame(QFrame):
         self.mousePressEvent = self.room_frame_clicked
         self.setCursor(QCursor(Qt.PointingHandCursor))
 
-        self.room_title_label = QLabel(self.room_title)
-        self.room_title_label.setObjectName("roomTitle")
+        self.name_label = QLabel(self.room.name)
+        self.name_label.setObjectName("roomTitle")
 
         self.vbox = QVBoxLayout()
         self.vbox.setContentsMargins(0, 0, 0, 0)
-        self.vbox.addWidget(self.room_title_label)
+        self.vbox.addWidget(self.name_label)
 
-        self.room_header_widget = QWidget()
-        self.room_header_widget.setObjectName("header")
-        self.room_header_widget.setLayout(self.vbox)
+        self.header_widget = QWidget()
+        self.header_widget.setObjectName("header")
+        self.header_widget.setLayout(self.vbox)
 
-        self.room_code_label = QLabel(self.room_code)
-        self.room_code_label.setObjectName("code")
-        self.room_time_label = QLabel(self.room_time)
-        self.room_time_label.setObjectName("time")
+        self.code_label = QLabel(self.room.code)
+        self.code_label.setObjectName("code")
+        self.time_label = QLabel(self.room.time)
+        self.time_label.setObjectName("time")
 
-        days_string = " ".join(self.room_days_list)
+        days_string = " ".join(self.room.days_list)
 
-        self.room_day_label = QLabel(days_string)
-        self.room_day_label.setObjectName("days")
+        self.days_label = QLabel(days_string)
+        self.days_label.setObjectName("days")
 
         self.body_vbox = QVBoxLayout()
         self.body_vbox.setContentsMargins(0, 0, 0, 0)
-        self.body_vbox.addWidget(self.room_code_label)
-        self.body_vbox.addWidget(self.room_time_label)
-        self.body_vbox.addWidget(self.room_day_label)
+        self.body_vbox.addWidget(self.code_label)
+        self.body_vbox.addWidget(self.time_label)
+        self.body_vbox.addWidget(self.days_label)
 
-        self.room_body_widget = QWidget()
-        self.room_body_widget.setObjectName("body")
-        self.room_body_widget.setLayout(self.body_vbox)
+        self.body_widget = QWidget()
+        self.body_widget.setObjectName("body")
+        self.body_widget.setLayout(self.body_vbox)
 
-        self.frame_layout = QVBoxLayout()
-        self.frame_layout.setContentsMargins(0, 0, 0, 0)
+        self.vbox_layout = QVBoxLayout()
+        self.vbox_layout.setContentsMargins(0, 0, 0, 0)
 
-        self.frame_layout.addWidget(self.room_header_widget)
-        self.frame_layout.addWidget(self.room_body_widget)
-        self.frame_layout.addStretch()
+        self.vbox_layout.addWidget(self.header_widget)
+        self.vbox_layout.addWidget(self.body_widget)
+        self.vbox_layout.addStretch()
 
-        self.setLayout(self.frame_layout)
+        self.setLayout(self.vbox_layout)
 
     def room_frame_clicked(self, mouse_click):
 
@@ -108,18 +113,13 @@ class EditRoomFrame(QFrame):
         self.edit_room_window.move(global_point)
         self.edit_room_window.show()
 
-        self.edit_room_window.room_name_input.setText(self.room_title)
-        self.edit_room_window.room_code_input.setText(self.room_code)
+        self.edit_room_window.name_input.setText(self.room.name)
+        self.edit_room_window.code_input.setText(self.room.code)
 
-        time = QTime.fromString(self.room_time, "h:mm AP")
-        self.edit_room_window.room_time_input.setTime(time)
+        time = QTime.fromString(self.room.time, "h:mm AP")
+        self.edit_room_window.time_input.setTime(time)
 
-        buttons = self.edit_room_window.button_group.buttons()
-
-        for i in range(len(self.room_days_list)):
-            for j in range(len(buttons)):
-                if buttons[j].text() == self.room_days_list[i]:
-                    buttons[j].setChecked(True)
+        self.edit_room_window.days_input.check_buttons(self.room.days_list)
 
 
 class AddRoomFrame(QFrame):
@@ -136,7 +136,6 @@ class AddRoomFrame(QFrame):
         self.setObjectName("main")
 
         self.setStyleSheet("""
-
         QFrame#main {
                 background-color: white;
                 border: 1px solid gray;
@@ -147,7 +146,6 @@ class AddRoomFrame(QFrame):
         QLabel#icon {
                 font-size: 30px
             }
-
         """)
 
         self.add_icon_label = QLabel("+")
@@ -163,15 +161,63 @@ class AddRoomFrame(QFrame):
         self.setLayout(self.add_room_frame_layout)
 
     def add_room_clicked(self, mouse_click):
-        print("Add Room")
+        """opens window getting user inputs for room creation"""
 
         self.add_room_window = AddRoomWindow()
 
         point = QPoint(50, 50)
         global_point = self.parentWidget().mapToGlobal(point)
-        self.add_room_window.move(global_point)
 
+        self.add_room_window.move(global_point)
         self.add_room_window.show()
+
+
+class DaysWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.button_group = QButtonGroup()
+        self.button_group.setExclusive(False)
+
+        hbox = QHBoxLayout()
+        hbox.setContentsMargins(0, 0, 0, 0)
+
+        days = ["M", "T", "W", "Th", "F", "Sa", "Su"]
+        for i in range(len(days)):
+            day_button = QPushButton(days[i])
+            self.button_group.addButton(day_button, i)
+            day_button.setObjectName("day")
+            day_button.setCheckable(True)
+            day_button.clicked.connect(lambda: self.change_color(day_button))
+            hbox.addWidget(day_button)
+
+        self.setLayout(hbox)
+
+    @staticmethod
+    def change_color(button):
+
+        if button.isChecked():
+            button.setStyleSheet("background-color: light blue")
+        else:
+            button.setStyleSheet("background-color: light gray")
+
+    def checked_buttons_text(self):
+
+        days = []
+        for button in self.button_group.buttons():
+            if button.isChecked():
+                days.append(button.text())
+
+        return days
+
+    def check_buttons(self, days_list):
+
+        buttons = self.button_group.buttons()
+
+        for i in range(len(days_list)):
+            for j in range(len(buttons)):
+                if buttons[j].text() == days_list[i]:
+                    buttons[j].setChecked(True)
 
 
 class RoomWindow(QWidget):
@@ -182,30 +228,37 @@ class RoomWindow(QWidget):
         self.header_title = header_title
         self.setObjectName("main")
         self.setStyleSheet("""
+            QWidget#main {
+                background-color: white;
+                border: 1px solid gray;
+            }
 
-                    QWidget#main {
-                        background-color: white;
-                        border: 1px solid gray;
-                    }
+            QLabel#head {
+                font-size: 20px;
+            }
 
-                    QLabel#head {
-                        font-size: 20px;
-                    }
+            QPushButton#day {
+                background-color: light gray;
+            }
+        """)
 
-                    QPushButton#day {
-                        background-color: light gray;
-                    }
-
-                """)
         self.setAttribute(Qt.WA_StyledBackground)
 
         self.setFixedSize(QSize(400, 300))
         flags = Qt.WindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setWindowFlags(flags)
 
-        head_label = QLabel(self.header_title)
-        head_label.setMaximumHeight(20)
-        head_label.setObjectName("head")
+        # locks main window until user exits info widget
+        self.setWindowModality(Qt.ApplicationModal)
+
+        self.create_contents()
+
+    def create_contents(self):
+
+        # HEADER SECTION
+        self.head_label = QLabel(self.header_title)
+        self.head_label.setMaximumHeight(20)
+        self.head_label.setObjectName("head")
 
         self.delete_room_button = QPushButton()
         self.delete_room_button.setMaximumWidth(30)
@@ -217,46 +270,32 @@ class RoomWindow(QWidget):
         # button is hidden by default
         self.delete_room_button.hide()
 
-        head_hbox = QHBoxLayout()
-        head_hbox.addWidget(head_label)
-        head_hbox.addWidget(self.delete_room_button)
+        self.head_hbox = QHBoxLayout()
+        self.head_hbox.addWidget(self.head_label)
+        self.head_hbox.addWidget(self.delete_room_button)
 
-        self.room_name_label = QLabel("Room Name: ")
-        self.room_name_input = QLineEdit()
+        # FORM SECTION
+        self.name_label = QLabel("Name:")
+        self.name_input = QLineEdit()
 
-        self.room_code_label = QLabel("Room Code: ")
-        self.room_code_input = QLineEdit()
+        self.code_label = QLabel("Code:")
+        self.code_input = QLineEdit()
 
-        self.room_time_label = QLabel("Room Time: ")
-        self.room_time_input = QTimeEdit()
+        self.time_label = QLabel("Time:")
+        self.time_input = QTimeEdit()
 
-        self.room_day_label = QLabel("Room Day: ")
-
-        hbox = QHBoxLayout()
-        hbox.setContentsMargins(0, 0, 0, 0)
-
-        self.button_group = QButtonGroup()
-        self.button_group.setExclusive(False)
-
-        days = ["M", "T", "W", "Th", "F", "Sa", "Su"]
-        for i in range(len(days)):
-            day_button = QPushButton(days[i])
-            self.button_group.addButton(day_button, i)
-            day_button.setObjectName("day")
-            day_button.setCheckable(True)
-            day_button.clicked.connect(lambda: self.change_color(day_button))
-            hbox.addWidget(day_button)
-
-        self.room_day_input = QWidget()
-        self.room_day_input.setLayout(hbox)
+        self.room_day_label = QLabel("Days:")
+        self.days_input = DaysWidget()
 
         form_layout = QFormLayout()
         form_layout.setVerticalSpacing(10)
-        form_layout.addRow(self.room_name_label, self.room_name_input)
-        form_layout.addRow(self.room_code_label, self.room_code_input)
-        form_layout.addRow(self.room_time_label, self.room_time_input)
-        form_layout.addRow(self.room_day_label, self.room_day_input)
+        form_layout.setHorizontalSpacing(20)
+        form_layout.addRow(self.name_label, self.name_input)
+        form_layout.addRow(self.code_label, self.code_input)
+        form_layout.addRow(self.time_label, self.time_input)
+        form_layout.addRow(self.room_day_label, self.days_input)
 
+        # BOTTOM SECTION
         save_button = QPushButton("Save")
         save_button.clicked.connect(self.save_info)
 
@@ -267,50 +306,51 @@ class RoomWindow(QWidget):
         button_hbox.addWidget(save_button)
         button_hbox.addWidget(cancel_button)
 
+        # OVERALL LAYOUT
         vbox = QVBoxLayout()
         vbox.addSpacing(4)
-        vbox.addLayout(head_hbox)
+        vbox.addLayout(self.head_hbox)
         vbox.addSpacing(40)
         vbox.addLayout(form_layout)
         vbox.addLayout(button_hbox)
 
         self.setLayout(vbox)
 
-        # locks main window until user exits info widget
-        self.setWindowModality(Qt.ApplicationModal)
-
-    def change_color(self, button):
-
-        if button.isChecked():
-            button.setStyleSheet("background-color: light blue")
-        else:
-            button.setStyleSheet("background-color: light gray")
-
     def save_info(self):
         room = self.get_info()
-
-        print("Saving Info...")
-        print(f"Room Name: {room['name']}")
-        print(f"Room Code: {room['code']}")
-        print(f"Room Time: {room['time']}")
-        print(f"Room Days: {room['days']}")
+        # TODO: put code to save info to database here
 
     def get_info(self):
-        inputs = self.findChildren(QLineEdit)
 
-        days = []
-        for button in self.button_group.buttons():
-            if button.isChecked():
-                days.append(button.text())
+        name = self.name_input.text()
+        code = self.code_input.text()
+        time = self.time_input.text()
+        days_list = self.days_input.checked_buttons_text()
 
-        room = {
-            "name": self.room_name_input.text(),
-            "code": self.room_code_input.text(),
-            "time": self.room_time_input.text(),
-            "days": days
-        }
+        room = Room(name, code, time, days_list)
 
         return room
+
+    @staticmethod
+    def get_position(count):
+        """
+        finds position to add frame to grid layout (2 column grid)
+        :param count: nth widget to be added
+        :return: position tuple
+        """
+
+        row = math.ceil(count / 2) - 1
+
+        num = count / 2
+
+        if num.is_integer():
+            column = 1
+        else:
+            column = 0
+
+        position = (row, column)
+
+        return position
 
     def close_window(self):
         self.hide()
@@ -330,72 +370,48 @@ class EditRoomWindow(RoomWindow):
     def save_info(self):
         room = self.get_info()
 
-        print(room["name"], room["code"], room["time"], room["days"])
-
         # Updates attributes of EditRoomFrame instance
-        self.edit_room_frame.room_title = room["name"]
-        self.edit_room_frame.room_code = room["code"]
-        self.edit_room_frame.room_time = room["time"]
-        self.edit_room_frame.room_days_list = room["days"]
+        self.edit_room_frame.room = room
 
-        self.edit_room_frame.room_title_label.setText(room["name"])
-        self.edit_room_frame.room_code_label.setText(room["code"])
-        self.edit_room_frame.room_time_label.setText(room["time"])
+        # Updates labels of EditRoomFrame instance
+        self.edit_room_frame.name_label.setText(room.name)
+        self.edit_room_frame.code_label.setText(room.code)
+        self.edit_room_frame.time_label.setText(room.time)
 
-        days_string = " ".join(room["days"])
+        days_string = " ".join(room.days_list)
+        self.edit_room_frame.days_label.setText(days_string)
 
-        self.edit_room_frame.room_day_label.setText(days_string)
+        # TODO: add code to save update info on database
 
         self.close_window()
 
     def delete_frame(self):
+
+        # deletes current EditRoomFrame but leaves a 'hole' in the GridLayout
         self.edit_room_frame.deleteLater()
 
+        # gets list of all EditRoomFrame instances in MainWindow
         parent_widget = self.edit_room_frame.parentWidget()
-
         edit_room_frames_list = parent_widget.findChildren(EditRoomFrame)
-
         edit_room_frames_list.remove(self.edit_room_frame)
 
+        # deletes all EditRoomFrame instances and re-adds all of them
+        # removing that 'hole' if the deleted frame was in the middle
         for frame in edit_room_frames_list:
             frame.deleteLater()
 
         children_count = 2
 
         for frame in edit_room_frames_list:
-            print(frame.room_title_label.text())
-
-        for frame in edit_room_frames_list:
             pos = self.get_position(children_count)
 
-            room = {
-                "name": frame.room_title_label.text(),
-                "code": frame.room_code_label.text(),
-                "time": frame.room_time_label.text(),
-                "days": frame.room_day_label.text().split()
-            }
+            room = frame.room
 
             window.grid_layout.addWidget(
-                EditRoomFrame(room["name"], room["code"], room["time"], room["days"]), pos[0], pos[1])
+                EditRoomFrame(room), pos[0], pos[1])
             children_count += 1
 
         self.close_window()
-
-    @staticmethod
-    def get_position(count):
-
-        row = math.ceil(count / 2) - 1
-
-        num = count / 2
-
-        if num.is_integer():
-            column = 1
-        else:
-            column = 0
-
-        position = (row, column)
-
-        return position
 
 
 class AddRoomWindow(RoomWindow):
@@ -407,7 +423,7 @@ class AddRoomWindow(RoomWindow):
     def save_info(self):
         room = self.get_info()
 
-        edit_frame = EditRoomFrame(room["name"], room["code"], room["time"], room["days"])
+        edit_frame = EditRoomFrame(room)
 
         grid_layout = window.grid_layout
 
@@ -415,30 +431,10 @@ class AddRoomWindow(RoomWindow):
         children_count += len(window.findChildren(AddRoomFrame))
         children_count += 1
 
-        print(children_count)
-
         pos = self.get_position(children_count)
-        print(pos)
-
         grid_layout.addWidget(edit_frame, pos[0], pos[1])
 
         self.close_window()
-
-    @staticmethod
-    def get_position(count):
-
-        row = math.ceil(count/2) - 1
-
-        num = count / 2
-
-        if num.is_integer():
-            column = 1
-        else:
-            column = 0
-
-        position = (row, column)
-
-        return position
 
 
 class MainWindow(QMainWindow):
@@ -447,28 +443,26 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         stylesheet = """
-
             QScrollArea {
                 background-color: white
             }
-
         """
 
         self.setFixedHeight(400)
         self.setMinimumWidth(520)
         self.setWindowTitle("Google Meet Auto Joiner")
         self.setStyleSheet(stylesheet)
-
         self.create_window_contents()
 
     def create_window_contents(self):
-        self.add_room_frame = AddRoomFrame()
-
         self.grid_layout = QGridLayout()
         self.grid_layout.setVerticalSpacing(0)
         self.grid_layout.setContentsMargins(0, 10, 0, 0)
 
+        self.add_room_frame = AddRoomFrame()
         self.grid_layout.addWidget(self.add_room_frame, 0, 0)
+
+        # blank widget to keep grid with 2 columns at initial
         self.grid_layout.addWidget(QWidget(), 0, 1)
 
         vbox = QVBoxLayout()
